0387194787947437f587f233c1d4f809ce524a12
KSkwarczynski <45295406+KSkwarczynski@users.noreply.github.com>
2018-11-25 20:39:54 +0100

    Add files via upload

---
+++ sFGD/include/SteppingAction.hh
@@ -0,0 +1,22 @@
+//
+//
+//
+/// \file SteppingAction.hh
+/// \brief Definition of the SteppingAction class
+//
+#ifndef SteppingAction_H
+#define SteppingACtion_H 1
+
+#include "G4UserSteppingAction.hh"
+
+class SteppingAction : public G4UserSteppingAction
+{
+  public:
+    SteppingAction();
+    virtual ~SteppingAction();
+    virtual void UserSteppingAction(const G4Step*); 
+  private:
+     void PrintStep(const G4Step*);
+};
+
+#endif
